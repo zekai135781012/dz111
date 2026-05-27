b1456288b09aa483ee1c5d364f8b2445542ac13c
zekai135781012 <167621470+zekai135781012@users.noreply.github.com>
2026-05-27 16:06:47 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,226 +1,126 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request, event.env));
-});
-
-async function handleRequest(request, env) {
-  var corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type'
-  };
-
-  if (request.method === 'OPTIONS') {
-    return new Response(null, { headers: corsHeaders });
-  }
-
-  var url = new URL(request.url);
-  if (url.pathname !== '/api/farm/latest') {
-    return makeJson({ success: false, error: 'Not Found' }, 404);
-  }
-
-  try {
-    var roleId = env.FARM_ROLE_ID;
-    var deviceModel = env.FARM_DEVICE_MODEL;
-    var uuid = env.FARM_UUID;
-    var server = env.FARM_SERVER || '15001';
-    var token = env.FARM_TOKEN || 'aU0ZcOzmpNoa56fDez';
-
-    if (!roleId || !deviceModel || !uuid) {
-      return makeJson({ success: false, error: '服务器配置不完整' }, 500);
-    }
-
-    var auth = generateAuth(roleId, token);
-
-    var body = JSON.stringify({
-      server: server,
-      code: 'u5',
-      sign: auth.sign,
-      language: 'zh-CN',
-      deviceName: 'duchamp',
-      systemVersion: 36,
-      uuid: uuid,
-      mode: 'view',
-      systemName: 'android',
-      batteryState: 3,
-      extra: '',
-      appId: '4608997350',
-      batteryLevel: 90,
-      gameId: 'u5',
-      roleId: roleId,
-      deeplink: '[{"scheme":"ntes","host":"game.mobile","pathPrefix":"/party"}]',
-      env: 'production',
-      nonce: auth.nonce,
-      size: 'medium',
-      domain: 'https://u5-vision.nie.netease.com',
-      designId: 4608997351,
-      sdkVersion: 3,
-      deviceModel: deviceModel,
-      ts: auth.ts,
-      nightMode: false
-    });
-
-    var resp = await fetch('https://u5-vision.nie.netease.com/widget/view', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Dalvik/2.1.0'
-      },
-      body: body
-    });
-
-    var json = await resp.json();
-
-    if (json.success !== 'true') {
-      return makeJson({ success: false, error: json.desc || '请求失败' }, 500);
-    }
-
-    var elements = {};
-    if (json.data && json.data.view && json.data.view.elements) {
-      elements = json.data.view.elements;
-    }
-
-    var weatherIcons = [];
-    if (elements['image_R3Xw:R5xp'] && elements['image_R3Xw:R5xp'].src) {
-      weatherIcons.push(elements['image_R3Xw:R5xp'].src);
-    }
-    if (elements['image_JeHp:Vmcb'] && elements['image_JeHp:Vmcb'].src) {
-      weatherIcons.push(elements['image_JeHp:Vmcb'].src);
-    }
-
-    var farmData = {
-      weatherIcons: weatherIcons,
-      seedImage: safeGet(elements, 'image_Lb2J:5Fo7', 'src'),
-      seedName: safeGet(elements, 'text_TLl5:dwdi', 'content'),
-      seedQualityBg: safeGet(elements, 'image_z9Sa:fdFU', 'src'),
-      toolImage: safeGet(elements, 'image_cruF:gFs5', 'src'),
-      toolName: safeGet(elements, 'text_vtOZ:50mh', 'content'),
-      toolQualityBg: safeGet(elements, 'image_6grb:EYVa', 'src'),
-      fetchTime: Date.now()
+export default {
+  async fetch(request, env, ctx) {
+    var cors = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
     };
-
-    return makeJson({
-      success: true,
-      data: farmData,
-      maskedRoleId: roleId.substring(0, 3) + '***',
-      maskedDeviceModel: '***',
-      maskedUuid: uuid.substring(0, 4) + '***'
-    });
-
-  } catch (e) {
-    return makeJson({ success: false, error: e.message }, 500);
-  }
-}
-
-function safeGet(obj, key, field) {
-  if (obj[key] && obj[key][field]) {
-    return obj[key][field];
-  }
-  return '';
-}
-
-function makeJson(data, status) {
-  return new Response(JSON.stringify(data), {
-    status: status || 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+    if (request.method === 'OPTIONS') {
+      return new Response(null, { headers: cors });
     }
+    var url = new URL(request.url);
+    if (url.pathname !== '/api/farm/latest') {
+      return jr({ success: false, error: 'Not Found' }, 404);
+    }
+    try {
+      var roleId = env.FARM_ROLE_ID;
+      var deviceModel = env.FARM_DEVICE_MODEL;
+      var uuid = env.FARM_UUID;
+      var token = env.FARM_TOKEN || 'aU0ZcOzmpNoa56fDez';
+      if (!roleId || !deviceModel || !uuid) {
+        return jr({ success: false, error: '配置不完整' }, 500);
+      }
+      var auth = ga(roleId, token);
+      var body = JSON.stringify({
+        server: '15001', code: 'u5', sign: auth.sign,
+        language: 'zh-CN', deviceName: 'duchamp', systemVersion: 36,
+        uuid: uuid, mode: 'view', systemName: 'android',
+        batteryState: 3, extra: '', appId: '4608997350',
+        batteryLevel: 90, gameId: 'u5', roleId: roleId,
+        deeplink: '[]', env: 'production', nonce: auth.nonce,
+        size: 'medium', domain: 'https://u5-vision.nie.netease.com',
+        designId: 4608997351, sdkVersion: 3, deviceModel: deviceModel,
+        ts: auth.ts, nightMode: false
+      });
+      var resp = await fetch('https://u5-vision.nie.netease.com/widget/view', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: body
+      });
+      var json = await resp.json();
+      if (json.success !== 'true') {
+        return jr({ success: false, error: json.desc || '失败' }, 500);
+      }
+      var el = json.data && json.data.view && json.data.view.elements ? json.data.view.elements : {};
+      var wi = [];
+      if (el['image_R3Xw:R5xp'] && el['image_R3Xw:R5xp'].src) wi.push(el['image_R3Xw:R5xp'].src);
+      if (el['image_JeHp:Vmcb'] && el['image_JeHp:Vmcb'].src) wi.push(el['image_JeHp:Vmcb'].src);
+      return jr({
+        success: true,
+        data: {
+          weatherIcons: wi,
+          seedImage: gs(el, 'image_Lb2J:5Fo7'),
+          seedName: gt(el, 'text_TLl5:dwdi'),
+          seedQualityBg: gs(el, 'image_z9Sa:fdFU'),
+          toolImage: gs(el, 'image_cruF:gFs5'),
+          toolName: gt(el, 'text_vtOZ:50mh'),
+          toolQualityBg: gs(el, 'image_6grb:EYVa'),
+          fetchTime: Date.now()
+        },
+        maskedRoleId: roleId.substring(0, 3) + '***',
+        maskedDeviceModel: '***',
+        maskedUuid: uuid.substring(0, 4) + '***'
+      });
+    } catch (e) {
+      return jr({ success: false, error: e.message }, 500);
+    }
+  }
+};
+
+function gs(o, k) { return o[k] && o[k].src ? o[k].src : ''; }
+function gt(o, k) { return o[k] && o[k].content ? o[k].content : ''; }
+function jr(d, s) {
+  return new Response(JSON.stringify(d), {
+    status: s || 200,
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
   });
 }
 
-function md5(str) {
-  var hc = '0123456789abcdef';
-  function rh(n) {
-    var j, s = '';
-    for (j = 0; j < 4; j++) {
-      s += hc.charAt((n >> (j * 8 + 4)) & 0x0F) + hc.charAt((n >> (j * 8)) & 0x0F);
-    }
-    return s;
-  }
-  function ad(x, y) {
-    var l = (x & 0xFFFF) + (y & 0xFFFF);
-    var m = (x >> 16) + (y >> 16) + (l >> 16);
-    return (m << 16) | (l & 0xFFFF);
-  }
-  function rl(n, c) {
-    return (n << c) | (n >>> (32 - c));
-  }
-  function cm(q, a, b, x, s, e) {
-    return ad(rl(ad(ad(a, q), ad(x, e)), s), b);
-  }
-  function ff(a, b, c, d, x, s, e) {
-    return cm((b & c) | ((~b) & d), a, b, x, s, e);
-  }
-  function gg(a, b, c, d, x, s, e) {
-    return cm((b & d) | (c & (~d)), a, b, x, s, e);
-  }
-  function hh(a, b, c, d, x, s, e) {
-    return cm(b ^ c ^ d, a, b, x, s, e);
-  }
-  function ii(a, b, c, d, x, s, e) {
-    return cm(c ^ (b | (~d)), a, b, x, s, e);
-  }
-  var i;
-  var nblk = ((str.length + 8) >> 6) + 1;
-  var blks = new Array(nblk * 16);
-  for (i = 0; i < nblk * 16; i++) blks[i] = 0;
-  for (i = 0; i < str.length; i++) blks[i >> 2] |= str.charCodeAt(i) << ((i % 4) * 8);
-  blks[i >> 2] |= 0x80 << ((i % 4) * 8);
-  blks[nblk * 16 - 2] = str.length * 8;
-  var a = 1732584193, b = -271733879, c = -1732584194, d = 271733878;
-  var olda, oldb, oldc, oldd;
-  for (i = 0; i < blks.length; i += 16) {
-    olda = a; oldb = b; oldc = c; oldd = d;
-    a = ff(a, b, c, d, blks[i+0], 7, -680876936); d = ff(d, a, b, c, blks[i+1], 12, -389564586);
-    c = ff(c, d, a, b, blks[i+2], 17, 606105819); b = ff(b, c, d, a, blks[i+3], 22, -1044525330);
-    a = ff(a, b, c, d, blks[i+4], 7, -176418897); d = ff(d, a, b, c, blks[i+5], 12, 1200080426);
-    c = ff(c, d, a, b, blks[i+6], 17, -1473231341); b = ff(b, c, d, a, blks[i+7], 22, -45705983);
-    a = ff(a, b, c, d, blks[i+8], 7, 1770035416); d = ff(d, a, b, c, blks[i+9], 12, -1958414417);
-    c = ff(c, d, a, b, blks[i+10], 17, -42063); b = ff(b, c, d, a, blks[i+11], 22, -1990404162);
-    a = ff(a, b, c, d, blks[i+12], 7, 1804603682); d = ff(d, a, b, c, blks[i+13], 12, -40341101);
-    c = ff(c, d, a, b, blks[i+14], 17, -1502002290); b = ff(b, c, d, a, blks[i+15], 22, 1236535329);
-    a = gg(a, b, c, d, blks[i+1], 5, -165796510); d = gg(d, a, b, c, blks[i+6], 9, -1069501632);
-    c = gg(c, d, a, b, blks[i+11], 14, 643717713); b = gg(b, c, d, a, blks[i+0], 20, -373897302);
-    a = gg(a, b, c, d, blks[i+5], 5, -701558691); d = gg(d, a, b, c, blks[i+10], 9, 38016083);
-    c = gg(c, d, a, b, blks[i+15], 14, -660478335); b = gg(b, c, d, a, blks[i+4], 20, -405537848);
-    a = gg(a, b, c, d, blks[i+9], 5, 568446438); d = gg(d, a, b, c, blks[i+14], 9, -1019803690);
-    c = gg(c, d, a, b, blks[i+3], 14, -187363961); b = gg(b, c, d, a, blks[i+8], 20, 1163531501);
-    a = gg(a, b, c, d, blks[i+13], 5, -1444681467); d = gg(d, a, b, c, blks[i+2], 9, -51403784);
-    c = gg(c, d, a, b, blks[i+7], 14, 1735328473); b = gg(b, c, d, a, blks[i+12], 20, -1926607734);
-    a = hh(a, b, c, d, blks[i+5], 4, -378558); d = hh(d, a, b, c, blks[i+8], 11, -2022574463);
-    c = hh(c, d, a, b, blks[i+11], 16, 1839030562); b = hh(b, c, d, a, blks[i+14], 23, -35309556);
-    a = hh(a, b, c, d, blks[i+1], 4, -1530992060); d = hh(d, a, b, c, blks[i+4], 11, 1272893353);
-    c = hh(c, d, a, b, blks[i+7], 16, -155497632); b = hh(b, c, d, a, blks[i+10], 23, -1094730640);
-    a = hh(a, b, c, d, blks[i+13], 4, 681279174); d = hh(d, a, b, c, blks[i+0], 11, -358537222);
-    c = hh(c, d, a, b, blks[i+3], 16, -722521979); b = hh(b, c, d, a, blks[i+6], 23, 76029189);
-    a = hh(a, b, c, d, blks[i+9], 4, -640364487); d = hh(d, a, b, c, blks[i+12], 11, -421815835);
-    c = hh(c, d, a, b, blks[i+15], 16, 530742520); b = hh(b, c, d, a, blks[i+2], 23, -995338651);
-    a = ii(a, b, c, d, blks[i+0], 6, -198630844); d = ii(d, a, b, c, blks[i+7], 10, 1126891415);
-    c = ii(c, d, a, b, blks[i+14], 15, -1416354905); b = ii(b, c, d, a, blks[i+5], 21, -57434055);
-    a = ii(a, b, c, d, blks[i+12], 6, 1700485571); d = ii(d, a, b, c, blks[i+3], 10, -1894986606);
-    c = ii(c, d, a, b, blks[i+10], 15, -1051523); b = ii(b, c, d, a, blks[i+1], 21, -2054922799);
-    a = ii(a, b, c, d, blks[i+8], 6, 1873313359); d = ii(d, a, b, c, blks[i+15], 10, -30611744);
-    c = ii(c, d, a, b, blks[i+6], 15, -1560198380); b = ii(b, c, d, a, blks[i+13], 21, 1309151649);
-    a = ii(a, b, c, d, blks[i+4], 6, -145523070); d = ii(d, a, b, c, blks[i+11], 10, -1120210379);
-    c = ii(c, d, a, b, blks[i+2], 15, 718787259); b = ii(b, c, d, a, blks[i+9], 21, -343485551);
-    a = ad(a, olda); b = ad(b, oldb); c = ad(c, oldc); d = ad(d, oldd);
-  }
-  return rh(a) + rh(b) + rh(c) + rh(d);
-}
-
-function generateAuth(roleId, token) {
+function ga(roleId, token) {
   var ts = Date.now();
   var chars = '0123456789abcdefghijklmnopqrstuvwxyz';
   var nonce = '';
-  for (var i = 0; i < 18; i++) {
-    nonce += chars.charAt(Math.floor(Math.random() * 36));
+  for (var i = 0; i < 18; i++) nonce += chars.charAt(Math.floor(Math.random() * 36));
+  var now = new Date(ts + 28800000);
+  var ds = now.getUTCFullYear() + '' + String(now.getUTCMonth() + 1).padStart(2, '0') + '' + String(now.getUTCDate()).padStart(2, '0');
+  var dt = md5('code=u5&date=' + ds + '&token=' + token);
+  return { sign: md5('code=u5&roleId=' + roleId + '&nonce=' + nonce + '&ts=' + ts + '&token=' + dt), nonce: nonce, ts: ts };
+}
+
+function md5(s) {
+  function L(k, d) { return (k << d) | (k >>> (32 - d)); }
+  function K(G, k) { var I, d, F, H, x; F = (G & 2147483648); H = (k & 2147483648); I = (G & 1073741824); d = (k & 1073741824); x = (G & 1073741823) + (k & 1073741823); if (I & d) return (x ^ 2147483648 ^ F ^ H); if (I | d) { if (x & 1073741824) return (x ^ 3221225472 ^ F ^ H); else return (x ^ 1073741824 ^ F ^ H); } else return (x ^ F ^ H); }
+  function r(d, F, G) { return (d & F) | ((~d) & G); }
+  function q(d, F, G) { return (d & G) | (F & (~G)); }
+  function p(d, F, G) { return (d ^ F ^ G); }
+  function n(d, F, G) { return (F ^ (d | (~G))); }
+  function u(G, F, aa, Z, k, H, I) { G = K(G, K(K(r(F, aa, Z), k), I)); return K(L(G, H), F); }
+  function f(G, F, aa, Z, k, H, I) { G = K(G, K(K(q(F, aa, Z), k), I)); return K(L(G, H), F); }
+  function D(G, F, aa, Z, k, H, I) { G = K(G, K(K(p(F, aa, Z), k), I)); return K(L(G, H), F); }
+  function t(G, F, aa, Z, k, H, I) { G = K(G, K(K(n(F, aa, Z), k), I)); return K(L(G, H), F); }
+  function e(G) { var Z; var F = G.length; var x = F + 8; var k = (x - (x % 64)) / 64; var I = (k + 1) * 16; var aa = Array(I - 1); var d = 0; var H = 0; while (H < F) { Z = (H - (H % 4)) / 4; d = (H % 4) * 8; aa[Z] = (aa[Z] | (G.charCodeAt(H) << d)); H++; } Z = (H - (H % 4)) / 4; d = (H % 4) * 8; aa[Z] = aa[Z] | (128 << d); aa[I - 2] = F << 3; aa[I - 1] = F >>> 29; return aa; }
+  function B(x) { var k = "", F = "", G, d; for (d = 0; d <= 3; d++) { G = (x >>> (d * 8)) & 255; F = "0" + G.toString(16); k = k + F.substr(F.length - 2, 2); } return k; }
+  var C = []; var P, h, E, v, g, Y, X, W, V; var S = 7, Q = 12, N = 17, M = 22; var A = 5, z = 9, y = 14, w = 20; var o = 4, m = 11, l = 16, j = 23; var U = 6, T = 10, R = 15, O = 21;
+  s = utf8Encode(s); C = e(s); Y = 1732584193; X = 4023233417; W = 2562383102; V = 271733878;
+  for (P = 0; P < C.length; P += 16) { h = Y; E = X; g = W; v = V; Y = u(Y, X, W, V, C[P + 0], S, 3614090360); V = u(V, Y, X, W, C[P + 1], Q, 3905402710); W = u(W, V, Y, X, C[P + 2], N, 606105819); X = u(X, W, V, Y, C[P + 3], M, 3250441966); Y = u(Y, X, W, V, C[P + 4], S, 4118548399); V = u(V, Y, X, W, C[P + 5], Q, 1200080426); W = u(W, V, Y, X, C[P + 6], N, 2821735955); X = u(X, W, V, Y, C[P + 7], M, 4249261313); Y = u(Y, X, W, V, C[P + 8], S, 1770035416); V = u(V, Y, X, W, C[P + 9], Q, 2336552879); W = u(W, V, Y, X, C[P + 10], N, 4294925233); X = u(X, W, V, Y, C[P + 11], M, 2304563134); Y = u(Y, X, W, V, C[P + 12], S, 1804603682); V = u(V, Y, X, W, C[P + 13], Q, 4254626195); W = u(W, V, Y, X, C[P + 14], N, 2792965006); X = u(X, W, V, Y, C[P + 15], M, 1236535329); Y = f(Y, X, W, V, C[P + 1], A, 4129170786); V = f(V, Y, X, W, C[P + 6], z, 3225465664); W = f(W, V, Y, X, C[P + 11], y, 643717713); X = f(X, W, V, Y, C[P + 0], w, 3921069994); Y = f(Y, X, W, V, C[P + 5], A, 3593408605); V = f(V, Y, X, W, C[P + 10], z, 38016083); W = f(W, V, Y, X, C[P + 15], y, 3634488961); X = f(X, W, V, Y, C[P + 4], w, 3889429448); Y = f(Y, X, W, V, C[P + 9], A, 568446438); V = f(V, Y, X, W, C[P + 14], z, 3275163606); W = f(W, V, Y, X, C[P + 3], y, 4107603335); X = f(X, W, V, Y, C[P + 8], w, 1163531501); Y = f(Y, X, W, V, C[P + 13], A, 2850285829); V = f(V, Y, X, W, C[P + 2], z, 4243563512); W = f(W, V, Y, X, C[P + 7], y, 1735328473); X = f(X, W, V, Y, C[P + 12], w, 2368359562); Y = D(Y, X, W, V, C[P + 5], o, 4294588738); V = D(V, Y, X, W, C[P + 8], m, 2272392833); W = D(W, V, Y, X, C[P + 11], l, 1839030562); X = D(X, W, V, Y, C[P + 14], j, 4259657740); Y = D(Y, X, W, V, C[P + 1], o, 2763975236); V = D(V, Y, X, W, C[P + 4], m, 1272893353); W = D(W, V, Y, X, C[P + 7], l, 4139469664); X = D(X, W, V, Y, C[P + 10], j, 3200236656); Y = D(Y, X, W, V, C[P + 13], o, 681279174); V = D(V, Y, X, W, C[P + 0], m, 3936430074); W = D(W, V, Y, X, C[P + 3], l, 3572445317); X = D(X, W, V, Y, C[P + 6], j, 76029189); Y = D(Y, X, W, V, C[P + 9], o, 3654602809); V = D(V, Y, X, W, C[P + 12], m, 3873151461); W = D(W, V, Y, X, C[P + 15], l, 530742520); X = D(X, W, V, Y, C[P + 2], j, 3299628645); Y = t(Y, X, W, V, C[P + 0], U, 4096336452); V = t(V, Y, X, W, C[P + 7], T, 1126891415); W = t(W, V, Y, X, C[P + 14], R, 2878612391); X = t(X, W, V, Y, C[P + 5], O, 4237533241); Y = t(Y, X, W, V, C[P + 12], U, 1700485571); V = t(V, Y, X, W, C[P + 3], T, 2399980690); W = t(W, V, Y, X, C[P + 10], R, 4293915773); X = t(X, W, V, Y, C[P + 1], O, 2240044497); Y = t(Y, X, W, V, C[P + 8], U, 1873313359); V = t(V, Y, X, W, C[P + 15], T, 4264355552); W = t(W, V, Y, X, C[P + 6], R, 2734768916); X = t(X, W, V, Y, C[P + 13], O, 1309151649); Y = t(Y, X, W, V, C[P + 4], U, 4149444226); V = t(V, Y, X, W, C[P + 11], T, 3174756917); W = t(W, V, Y, X, C[P + 2], R, 718787259); X = t(X, W, V, Y, C[P + 9], O, 3951481745); Y = K(Y, h); X = K(X, E); W = K(W, g); V = K(V, v); }
+  return (B(Y) + B(X) + B(W) + B(V)).toLowerCase();
+}
+
+function utf8Encode(str) {
+  str = str.replace(/\r\n/g, "\n");
+  var utftext = "";
+  for (var n = 0; n < str.length; n++) {
+    var c = str.charCodeAt(n);
+    if (c < 128) {
+      utftext += String.fromCharCode(c);
+    } else if (c > 127 && c < 2048) {
+      utftext += String.fromCharCode((c >> 6) | 192);
+      utftext += String.fromCharCode((c & 63) | 128);
+    } else {
+      utftext += String.fromCharCode((c >> 12) | 224);
+      utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+      utftext += String.fromCharCode((c & 63) | 128);
+    }
   }
-  var now = new Date(ts + 8 * 3600 * 1000);
-  var y = now.getUTCFullYear();
-  var m = String(now.getUTCMonth() + 1).padStart(2, '0');
-  var d = String(now.getUTCDate()).padStart(2, '0');
-  var dateStr = y + '' + m + '' + d;
-  var dynamicToken = md5('code=u5&date=' + dateStr + '&token=' + token);
-  var sign = md5('code=u5&roleId=' + roleId + '&nonce=' + nonce + '&ts=' + ts + '&token=' + dynamicToken);
-  return { sign: sign, nonce: nonce, ts: ts };
+  return utftext;
 }
